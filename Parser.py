adee184c768e73263bf1f9c7570ace54ff947478
ManalHamdi <manal.hamdi@yahoo.fr>
2018-05-01 21:48:39 +0100

    Parser and programSub done

--- Parser.py
+++ Parser.py
@@ -1,8 +1,40 @@
 index_current_token = -1
-tokens = 
-def Lex():
+tokens = lexer()
+
+def lex():
 	index_current_token += 1
 	return tokens[index_current_token]
-def Parser():
 
-	 
+def parser():
+	nextToken = lex()
+	programSub(nextTokenPair)
+
+def programSub(nextTokenPair):
+	nextTokenType = nextTokenPair[0]
+
+	if(nextTokenType != "program"):
+		return "error"
+	nextTokenType = lex()
+	if(nextTokenType != "id"):
+		return "error"
+		#TREE
+	nextTokenType = lex()
+	if(nextTokenType != "lpar"):
+		return "error"
+
+	identifiersListSub()
+
+	nextTokenType = lex()
+	if (nextTokenType != 'rpar'):
+		return "error"
+	nextTokenType = lex()
+	if (nextTokenType != "scolon"):
+		return "error" 
+
+	declarationsSub()
+	subprogramDeclarationsSub()
+	compoundStatementsSub()
+	
+	nextTokenType = lex()
+	if (nextTokenType != "dot"):
+		return "error"
