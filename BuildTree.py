c9ab932c110b20e1d7ccfac0e4ae34acac42616c
Hamza Touhs <IDogueto@sse-sendidlap.aui.maincampus.root>
2018-05-03 19:23:28 +0100

    Add BuildTree

---
+++ BuildTree.py
@@ -0,0 +1,19 @@
+class NodeTree(object):
+    def __init__(self):
+        self.children = []
+        self.tokenType = None
+        self.tokenValue = None
+        self.parent = None
+        self.isTerminal = False
+
+
+def createNode (_tokenType, _tokenValue, _isTerminal):
+	node = NodeTree()
+	node.tokenValue = _tokenValue
+	node.tokenType = _tokenType
+	node.isTerminal = _isTerminal
+	return node
+
+def addNode (_parentNode, _node):
+	_node.parent = _parentNode
+	_parentNode.childen.append(_node)
